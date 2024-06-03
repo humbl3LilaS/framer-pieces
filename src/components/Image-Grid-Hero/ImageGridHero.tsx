31e096a78aaaaf9af74ece3c93be6861442c898f
humbl3LilaS <sabishinekobebe@gmail.com>
2024-06-03 20:07:17 +0630

    Fix: ImageGridHero layer precedence

--- src/components/Image-Grid-Hero/ImageGridHero.tsx
+++ src/components/Image-Grid-Hero/ImageGridHero.tsx
@@ -1,6 +1,6 @@
 import StickyNav from "@/components/Image-Grid-Hero/StickyNav.tsx";
 import {useRef} from "react";
-import {useScroll, useTransform} from "framer-motion";
+import {useScroll} from "framer-motion";
 import PhotoGrid from "@/components/Image-Grid-Hero/PhotoGrid.tsx";
 
 export default function ImageGridHero() {
@@ -10,11 +10,10 @@ export default function ImageGridHero() {
         offset: ["start start", "end end"],
     });
 
-    const bgTransition = useTransform(scrollYProgress, [0, 0.99, 1], ["#fff", "#fff", "#000"]);
-    const textTransition = useTransform(scrollYProgress, [0, 0.99, 1], ["#000", "#000", "#fff"]);
+
     return (
         <>
-            <StickyNav bgTransition={bgTransition} textTransition={textTransition}/>
+            <StickyNav scrollProgress={scrollYProgress}/>
             <section className={"w-screen h-[350vh] scroll-smooth"} ref={scrollRef}>
                 <div className={"relative w-full h-full"}>
                     <PhotoGrid scrollProgress={scrollYProgress}/>
--- src/components/Image-Grid-Hero/StickyNav.tsx
+++ src/components/Image-Grid-Hero/StickyNav.tsx
@@ -1,15 +1,18 @@
-import {motion, MotionValue} from "framer-motion";
+import {motion, MotionValue, useTransform} from "framer-motion";
 
 
-export default function StickyNav({bgTransition, textTransition}: { bgTransition: MotionValue<string> , textTransition: MotionValue<string>}) {
+export default function StickyNav({scrollProgress}: {scrollProgress : MotionValue<number>}) {
+
+    const background = useTransform(scrollProgress, [0, 0.99, 1], ["transparent", "transparent", "#000"]);
+    const color = useTransform(scrollProgress, [0, 0.99, 1], ["#031a19", "#031a19", "#fff"]);
 
     return (
         <header>
-            <motion.div className={"w-full h-16 p-4  fixed top-0 left-0 flex  justify-between items-center text-white bg-transparent"}
-                        style={{background: bgTransition}}
+            <motion.div className={"w-full h-16 p-4  fixed top-0 left-0 flex  justify-between items-center text-white bg-transparent z-40"}
+                        style={{background}}
 
             >
-                <motion.h1 className={"text-cyan-900 z-40"} style={{color: textTransition}}>
+                <motion.h1 className={"font-black text-xl tracking-wide"} style={{color}}>
                     Edelweiss
                 </motion.h1>
                 <button type={"button"} className={"px-4 py-2 bg-emerald-400 font-semibold "}>Sign In</button>
